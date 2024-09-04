cf46a244bca8ebc0b406031661324d2201c223bc
TingHuang222324 <t99635@gmail.com>
2024-09-04 15:54:43 +0800

    update page

--- assets/Logout-4114f165.js
+++
@@ -1,1 +0,0 @@
-import{ag as t,u as r,ah as s}from"./index-d757128d.js";const n={__name:"Logout",setup(u){const o=t(),e=r();return s(()=>{e.clearToken(),o.push({name:"Login"})}),()=>{}}};export{n as default};
